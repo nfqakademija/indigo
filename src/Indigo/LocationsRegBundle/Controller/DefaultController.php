0cb4f3ae6dcef25e66fdf683e7923e7cd961936b
Tadas Butė <tadas.bute@gmail.com>
2015-03-19 23:41:45 +0200

    created new bundle

---
+++ src/Indigo/LocationsRegBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Indigo\LocationsRegBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/new")
+     * @Template()
+     */
+    public function newAction()
+    {
+        return [];
+    }
+}
