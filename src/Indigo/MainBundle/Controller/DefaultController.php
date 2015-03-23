63ee3e4ed30fc0d99f75f6383b08ec337ce9f888
Darius Viciulis <darius.viciulis@gmail.com>
2015-03-23 02:21:54 +0200

    Fixed assets locations and updated  pages.min.css

--- src/Indigo/MainBundle/Controller/DefaultController.php
+++ src/Indigo/MainBundle/Controller/DefaultController.php
@@ -3,12 +3,14 @@
 namespace Indigo\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-//use Symfony\Bundle\AsseticBundle;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name=null)
+    public function indexAction($name)
     {
+        if (!$name) {
+            $name='index.html';
+        }
         return $this->render(sprintf('IndigoMainBundle:Pixel:%s.twig', $name), array("name" => $name));
     }
 }
