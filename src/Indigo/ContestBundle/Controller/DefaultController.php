e71260114f48127f618d8b6a0f0874c36aeef8c4
Tadas Butė <tadas.bute@gmail.com>
2015-03-26 16:46:00 +0200

    added contest create feature

--- src/Indigo/ContestBundle/Controller/DefaultController.php
+++ src/Indigo/ContestBundle/Controller/DefaultController.php
@@ -2,27 +2,65 @@
 
 namespace Indigo\ContestBundle\Controller;
 
+use Indigo\ContestBundle\Entity\Data;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/create_contest/{_locale}", defaults={"_locale" = "lt_LT"}, requirements={"_locale" = "en_US|lt_LT"})
+     * @Method({"GET", "POST"})
      * @Template()
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return [];
+        $data = new Data();
+
+        $form = $this->createFormBuilder($data)
+                ->add('contest_title', 'text', ['attr' => ['placeholder' => $this->st('create_contest.form.contest_title')], 'label' => false])
+                ->add('image', 'file', ['label' => false])
+                ->add('contest_privacy', 'checkbox', ['label' => $this->st('create_contest.form.contest_privacy'), 'required' => false])
+                ->add('contest_type', 'choice',
+                        ['label' => false,
+                        'choices' => [0 => $this->st('create_contest.form.contest_type.single'), 1 => $this->st('create_contest.form.contest_type.team')],
+                        'placeholder' => $this->st('create_contest.form.contest_type'),
+                        'required' => true])
+                ->add('table_name', 'text', ['attr' => ['placeholder' => $this->st('create_contest.form.table_name')], 'label' => false])
+                ->add('table_name', 'text', ['attr' => ['placeholder' => $this->st('create_contest.form.table_name')], 'label' => false])
+                ->add('save', 'submit', ['attr' => ['placeholder' => $this->st('create_contest.form.submit')], 'label' => false])
+                ->getForm();
+
+        if($request->isMethod("POST")){
+            $form->handleRequest($request);
+            if($form->isValid()) {
+                $this->dbQuery($data, $form->getData());
+            }
+            $return_data = $this->render('IndigoContestBundle:Default:index.html.twig',
+                array('contest_form' => $form->createView(), 'image_name' => $data->path_for_image
+                ));
+        }else{
+            $return_data = $this->render('IndigoContestBundle:Default:index.html.twig',
+                array('contest_form' => $form->createView(), 'image_name' => 'contest_logo.jpg'
+                ));
+        }
+
+        return $return_data;
     }
 
-    /**
-     * @Route("/new")
-     * @Template()
-     */
-    public function newAction()
-    {
-        return [];
+    //simplified translator
+    private function st($string){
+        return $this->get('translator')->trans($string);
+    }
+
+    //send data to database
+    private function dbQuery($data, $formData){
+        $em = $this->get('doctrine')->getManager();
+        $data->uploadImage();
+        $em->persist($formData);
+        $em->flush($formData);
     }
 }
---
+++ src/Indigo/ContestBundle/Entity/Data.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tadas
+ * Date: 2015-03-23
+ * Time: 16:41
+ */
+
+namespace Indigo\ContestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+
+/**
+ * Order
+ *
+ * @ORM\Table(name="contests")
+ * @ORM\Entity(repositoryClass="Indigo\ContestBundle\Controller\DefaultController\")
+ */
+class Data {
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned":true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank(groups={"Default"})
+     * @ORM\Column(name="contest_title", type="string", length=30, nullable=false)
+     */
+    private $contest_title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image_path", type="string", length=255, nullable=true)
+     */
+    public $path_for_image;
+
+    /**
+     * @Assert\File(maxSize="2000000")
+     */
+
+    private $image;
+
+    /**
+     * @var string
+     *
+     * @Assert\NotBlank(groups={"Default"})
+     * @ORM\Column(name="table_name", type="string", length=30, nullable=false)
+     */
+    private $table_name;
+
+    /**
+     * @var smallint
+     *
+     * @ORM\Column(name="contest_privacy", type="smallint", nullable=false)
+     */
+    private $contest_privacy;
+
+    /**
+     * @var smallint
+     *
+     * 0 = single, 1 = team
+     *
+     * @ORM\Column(name="contest_type", type="smallint", nullable=false)
+     */
+    private $contest_type;
+
+    /**
+     * @return int
+     */
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContestTitle(){
+        return $this->contest_title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPathForImage(){
+        return $this->getUploadDir().$this->path_for_image;
+    }
+
+    public function getUploadDir(){
+        return __DIR__.'/../../../../web/bundles/indigocontest/uploaded_images/contest/';
+    }
+
+    /**
+     * @return UploadedFile
+     */
+    public function getImage(){
+        return $this->image;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTableName(){
+        return $this->table_name;
+    }
+
+    /**
+     * @return smallint
+     */
+    public function getContestPrivacy(){
+        return $this->contest_privacy;
+    }
+
+    /**
+     * @return smallint
+     */
+    public function getContestType(){
+        return $this->contest_type;
+    }
+
+    /**
+    * @param string $contest_title
+    */
+    public function setContestTitle($contest_title){
+        $this->contest_title = $contest_title;
+    }
+
+    /**
+     * @param UploadedFile $image
+     */
+    public function setImage(UploadedFile $image = null){
+        $this->image = $image;
+    }
+
+    /**
+     * @param string $table_name
+     */
+    public function setTableName($table_name){
+        $this->table_name = $table_name;
+    }
+
+    /**
+     * @param smallint $contest_privacy
+     */
+    public function setContestPrivacy($contest_privacy){
+        $this->contest_privacy = $contest_privacy;
+    }
+
+    /**
+     * @param smallint $contest_type
+     */
+    public function setContestType($contest_type){
+        $this->contest_type = $contest_type;
+    }
+
+
+    public function uploadImage(){
+        if (null === $this->getImage()) {
+            return;
+        }
+
+        $this->getImage()->move(
+            $this->getPathForImage(),
+            $this->getImage()->getClientOriginalName()
+        );
+
+        $this->path_for_image = $this->getImage()->getClientOriginalName();
+
+        $this->image = null;
+    }
+
+}
