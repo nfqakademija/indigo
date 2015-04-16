4bd1f252c0cd7d20eaf724130a841a07f0a6cd06
TadasSurgailis <tadas.surgailis@gmail.com>
2015-04-16 13:59:47 +0300

    # Added missed files

---
+++ src/Indigo/UIBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Indigo\UIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('IndigoUIBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
