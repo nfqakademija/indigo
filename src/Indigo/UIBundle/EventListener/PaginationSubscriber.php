66390eea5e7c2ce5ed2c40881984255c28bcf178
Darius Viciulis <darius.viciulis@gmail.com>
2015-05-12 00:38:34 +0300

    Added pagination to constest games history.

---
+++ src/Indigo/UIBundle/EventListener/PaginationSubscriber.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simpleuser
+ * Date: 5/12/2015
+ * Time: 12:04 AM
+ */
+
+namespace Indigo\UIBundle\EventListener;
+
+
+class PaginationSubscriber {
+
+}
